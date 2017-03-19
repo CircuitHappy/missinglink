fcb6ebe48109bc6d34e5bd388f2f887c58f9b48d
Nick Donaldson <ndonald2@gmail.com>
2017-03-19 17:55:23 -0400

    [WIP] working sysfs GPIO, partial class-based impl

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
 
 int main(void) {
-    
-    return 0;
+
+  ofstream gpioExport;
+  gpioExport.open("/sys/class/gpio/export");
+  gpioExport << "132";
+  gpioExport.close();
+
+  ofstream pinDirection;
+  pinDirection.open("/sys/class/gpio/gpio132/direction");
+  pinDirection << "out";
+  pinDirection.close();
+
+  ofstream pinValue;
+  pinValue.open("/sys/class/gpio/gpio132/value");
+
+  bool value = false;
+  while (1) {
+    pinValue << (value ? "1" : "0");
+    pinValue.flush();
+    value = !value;
+    this_thread::sleep_for(chrono::seconds(1));
+  }
+
+  return 0;
 }
--- src/gpio/FilemappedPin.hpp
+++ src/gpio/FilemappedPin.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include "gpio/gpio_types.hpp"
 
 namespace MissingLink {
@@ -9,10 +10,21 @@ class FilemappedPin {
 public:
   FilemappedPin(int pinId);
   virtual ~FilemappedPin();
-
   void enable();
-private:
-  int m_pinId;
+protected:
+  static const std::string sysfsPrefix;
+  const int m_pinId;
 };
+
+//class FilemappedInput : public FilemappedPin {
+//public:
+//  GPIO::DigitalValue read() const;
+//};
+
+//class FilemappedOutput : public FilemappedPin {
+//public:
+//  FilemappedOutput(int pinId);
+//  void write(DigitalValue value) const;
+//};
 
 }}
