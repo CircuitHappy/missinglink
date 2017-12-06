72b63765e9b23e72aa95b3e76c529812f0bdece6
Nick Donaldson <ndonald2@gmail.com>
2017-12-05 20:26:27 -0500

    Clean up basic LED driver interface
    Remove unnecessary stuff and comment config steps

--- src/missing_link/led_driver.cpp
+++ src/missing_link/led_driver.cpp
@@ -15,10 +15,10 @@ using namespace MissingLink::GPIO;
 // Register address definitions
 enum LEDDriver::Register : uint8_t {
   MODE1     = 0x00, // Mode 1
-  LEDOUT0   = 0x14, // Input Polarity
-  LEDOUT1   = 0x15, // LED Output state 0
-  LEDOUT2   = 0x16, // LED Output state 1
-  LEDOUT3   = 0x17, // LED Output state 2
+  LEDOUT0   = 0x14, // LED Output state 0
+  LEDOUT1   = 0x15, // LED Output state 1
+  LEDOUT2   = 0x16, // LED Output state 2
+  LEDOUT3   = 0x17, // LED Output state 3
 };
 
 LEDDriver::LEDDriver(uint8_t i2cBus, uint8_t i2cAddress)
@@ -27,8 +27,10 @@ LEDDriver::LEDDriver(uint8_t i2cBus, uint8_t i2cAddress)
 
 LEDDriver::~LEDDriver() {}
 
-void LEDDriver::Configure(const Configuration &config) {
+void LEDDriver::Configure() {
+  // enable oscillator
   m_i2cDevice->WriteByte(MODE1, 0x04);
+  // enable all LED groups for individual and group PWM control
   m_i2cDevice->WriteByte(LEDOUT0, 0xff);
   m_i2cDevice->WriteByte(LEDOUT1, 0xff);
   m_i2cDevice->WriteByte(LEDOUT2, 0xff);
