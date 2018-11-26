8e11855c546fd39332fbf9611f0318a89ca5a497
Ed Guild <ed@vizzie.com>
2018-11-26 04:09:54 +0000

    added try/catch to opening MIDI port so ML does not crash.

--- src/missing_link/midi_out.cpp
+++ src/missing_link/midi_out.cpp
@@ -46,9 +46,14 @@ void MidiOut::open() {
     std::cout << "No MIDI ports available!\n";
     m_foundMidiPort = false;
   } else {
-    // Open first available port.
-    m_pMidiOut->openPort( 1 );
-    m_foundMidiPort = true;
+    // Open Port 1 which should be the USB MIDI adapter.
+    try {
+      m_pMidiOut->openPort( 1 );
+      m_foundMidiPort = true;
+    } catch (RtMidiError &error) {
+      error.printMessage();
+      m_foundMidiPort = false;
+    }
   }
 
 }
@@ -56,42 +61,3 @@ void MidiOut::open() {
 void MidiOut::close() {
   //close open MIDI port
 }
-
-// int main()
-// {
-//   RtMidiOut *midiout = new RtMidiOut();
-//   std::vector<unsigned char> message;
-//   // Check available ports.
-//   unsigned int nPorts = midiout->getPortCount();
-//   if ( nPorts == 0 ) {
-//     std::cout << "No ports available!\n";
-//     goto cleanup;
-//   }
-//   // Open first available port.
-//   midiout->openPort( 0 );
-//   // Send out a series of MIDI messages.
-//   // Program change: 192, 5
-//   message.push_back( 192 );
-//   message.push_back( 5 );
-//   midiout->sendMessage( &message );
-//   // Control Change: 176, 7, 100 (volume)
-//   message[0] = 176;
-//   message[1] = 7;
-//   message.push_back( 100 );
-//   midiout->sendMessage( &message );
-//   // Note On: 144, 64, 90
-//   message[0] = 144;
-//   message[1] = 64;
-//   message[2] = 90;
-//   midiout->sendMessage( &message );
-//   SLEEP( 500 ); // Platform-dependent ... see example in tests directory.
-//   // Note Off: 128, 64, 40
-//   message[0] = 128;
-//   message[1] = 64;
-//   message[2] = 40;
-//   midiout->sendMessage( &message );
-//   // Clean up
-//  cleanup:
-//   delete midiout;
-//   return 0;
-// }
