3b4ad0925fdc4244ae76e52b4e558230f7e015df
Ed Guild <ed@vizzie.com>
2019-06-30 19:05:54 +0000

    search uap0 if wlan0 returns no ip

--- src/missing_link/system_info.cpp
+++ src/missing_link/system_info.cpp
@@ -16,6 +16,7 @@ SysInfo::~SysInfo() {
 }
 
 std::string SysInfo::GetIP() {
+  bool foundWlan = false;
   FILE * fp = popen("ifconfig wlan0", "r");
   std::string result = "000.000.000.000";
   std::string line;
@@ -28,11 +29,30 @@ std::string SysInfo::GetIP() {
         int stop = line.find(" netmask") - start;
         result = line.substr(line.find("inet ") + 5, stop);
         std::cout << "WLAN IP: " + result << std::endl;
+        foundWlan = true;
         break;
       }
     }
+    pclose(fp);
   }
-  pclose(fp);
+  if (foundWlan == false) {
+    fp = popen("ifconfig uap0", "r");
+    if (fp) {
+      char *p=NULL; size_t n;
+      while ((getline(&p, &n, fp) > 0) && p) {
+        line = (std::string)p;
+        if (line.find("inet ") != std::string::npos) {
+          int start = line.find("inet ") + 5;
+          int stop = line.find(" netmask") - start;
+          result = line.substr(line.find("inet ") + 5, stop);
+          std::cout << "WLAN IP: " + result << std::endl;
+          foundWlan = true;
+          break;
+        }
+      }
+      pclose(fp);
+    }
+  }
   return result;
 }
 
